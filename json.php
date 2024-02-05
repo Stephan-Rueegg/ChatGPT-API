ad030697894141c07155df7440d4c86683b15c09
Stephan-Rueegg <157947419+Stephan-Rueegg@users.noreply.github.com>
2024-02-05 20:44:22 +0100

    Create json.php

---
+++ json.php
@@ -0,0 +1,14 @@
+{
+  "model": "gpt-3.5-turbo",
+  "messages": [
+    {
+      "role": "user",
+      "content": "recommend 1 classic song older than 100years, most modern"
+    }
+  ],
+  "temperature": 1,
+  "max_tokens": 256,
+  "top_p": 1,
+  "frequency_penalty": 0,
+  "presence_penalty": 0
+}
